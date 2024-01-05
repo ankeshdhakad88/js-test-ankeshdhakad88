069fa78a0eec175fbc2337014e2b3e1e9ac36914
Ankesh Verma <vermaankesh206@gmail.com>
2024-01-05 13:53:15 -0800

    submitted

--- program1.js
+++ program1.js
@@ -3,9 +3,34 @@
  * @return {boolean}
  */
 var isValid = function(s) {
+    if(s.length >=1 && s.length<= 104 )
+    {
+        const stack =[];
+        const parenthesisMap = {
+        '(' : ')',
+        '{' :'}',
+        '[' :']'    };
+        for(let char of s){
+            if(char in parenthesisMap){
+                stack.push(char);
+            }
+            else
+            {
+                const last = stack.pop();
+                if(char !==parenthesisMap[last])
+                {
+                    return false;
+                }
+            }
+        }
+        return stack.length ===0;
+    }
+    else
+    {
+        return false;
+    }
     
 };
 
 module.exports = { isValid };
-
 

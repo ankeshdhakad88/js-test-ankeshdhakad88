dec008e47b570a1c1664200a2ef6be3f02a1a864
Ankesh Verma <vermaankesh206@gmail.com>
2024-01-05 14:07:11 -0800

    Auto commit on save

--- program2.js
+++ program2.js
@@ -3,28 +3,27 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-    let result = '';
-    const map = {
-      M: 1000,
-      CM: 900,
-      D: 500,
-      CD: 400,
-      C: 100,
-      XC: 90,
-      L: 50,
-      XL: 40,
-      X: 10,
-      IX: 9,
-      V: 5,
-      IV: 4,
-      I: 1,
-    };
-  
-    for (key in map) {
-      result += key.repeat(Math.floor(num / map[key]));
-      num %= map[key];
+    let table = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000
     }
-  
+    
+    let result = 0;
+    for (let i = 0; i < s.length; i++) {
+        //if the next roman numeral is larger, then we know we have to subtract this number
+        if (table[s[i]] < table[s[i+1]]) {
+            result-=table[s[i]]
+        } 
+        //otherwise, add like normal. 
+        else {
+            result+=table[s[i]]
+        }
+    }
     return result
 };
 
